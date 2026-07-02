0048612891d00b0050f87716f48d1fdd392fa0f9
Abdelrahman Mriash <abeda9680@gmail.com>
2026-07-02 11:54:39 +0300

    Add Fiveth video

--- filament-app/app/Filament/Resources/Posts/Schemas/PostsForm.php
+++ filament-app/app/Filament/Resources/Posts/Schemas/PostsForm.php
@@ -10,27 +10,48 @@ use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Group;
 use App\Models\Category;
 use Filament\Forms\Components\Select;
+use Filament\Support\Icons\Heroicon;
 
 class PostsForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            
             ->components([
-                TextInput::make('title'),
-                TextInput::make('slug'),
-                Select::make('category_id')
-                    ->label('Category')
-                    ->options(Category::all()->pluck('name', 'id')),
-                    ColorPicker::make('color'),
-                    RichEditor::make('body'),
-                    FileUpload::make('image')->disk('public')->directory('posts'),
-                    TagsInput::make('tags'),
-                    Checkbox::make('Published'),
-                    DatePicker::make('published_at')
-            ]);
+                Section::make('Fields')
+                   ->description("Fill all fields in the form")
+                    ->icon(Heroicon::RocketLaunch)
+                    ->schema([    
+                        Group::make()
+                            ->schema([
+                                TextInput::make('title'),
+                                TextInput::make('slug'),
+                                Select::make('category_id')
+                                    ->label('Category')
+                                    ->options(Category::all()->pluck('name', 'id')),
+                                ColorPicker::make('color'),
+                                MarkdownEditor::make('body'),
+                            ])
+                    ])->columnSpan(2),   
+                    Group::make()
+                        ->schema([
+                            Section::make('Image Uploade')
+                                ->schema([
+                                    FileUpload::make('image')->disk('public')->directory('posts')
+                                ]),
+                            Section::make('Meta')
+                                ->schema([
+                                    TagsInput::make('tags'),
+                                    Checkbox::make('published'),
+                                    DatePicker::make('published_at')
+                                ])
+                    ])->columnSpan(1),
+                ])->columns(3);
     }
 }
