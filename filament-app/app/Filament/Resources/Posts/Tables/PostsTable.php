d3cef33fc97ac78ca593a17a04ff572dcf2ac11e
Abdelrahman Mriash <abeda9680@gmail.com>
2026-07-04 18:37:52 +0300

    Add the video number Eleven

--- filament-app/app/Filament/Resources/Posts/Tables/PostsTable.php
+++ filament-app/app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -8,6 +8,10 @@ use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Table;
 
 class PostsTable
@@ -17,17 +21,31 @@ class PostsTable
         return $table
             ->columns([
                 ImageColumn::make('image')->disk("public"),
-                TextColumn::make('title')->sortable(),
-                TextColumn::make('slug')->sortable(),
-                TextColumn::make('category.name')->sortable(),
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('slug')->sortable()->searchable(),
+                TextColumn::make('category.name')->sortable()->searchable(),
                 ColorColumn::make('color'),
                 TextColumn::make('created_at')
                     ->label('Created At')
                     ->dateTime()
                     ->sortable()
             ])->defaultSort('title', 'asc')
-            ->filters([
-                //
+                ->filters([
+                Filter::make('created_at')
+                    ->Label('Creation date')
+                    ->Schema([
+                        DatePicker::make('created_from')
+                            ->label('Select date'),
+                    ])->query(function ($query, array $data) {
+                        return $query
+                            ->when($data['created_from'], function ($q, $date) {
+                                $q->whereDate('created_at', $date);
+                            });
+                }),
+                SelectFilter::make('category_id')
+                    ->label('Select Category')
+                    ->relationship('category', 'name')
+                    ->preload(),
             ])
             ->recordActions([
                 EditAction::make(),
@@ -35,7 +53,8 @@ class PostsTable
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
-                ]),
+                ])    
+                
             ]);
     }
 }
