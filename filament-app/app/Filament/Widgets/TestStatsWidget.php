1be46f90ae72ac293c03384ea49313e2ca5e8cd9
Abdelrahman Mriash <abeda9680@gmail.com>
2026-07-10 13:24:01 +0300

    I have a problem here, I can't run this part code, please can you help me

--- filament-app/app/Filament/Widgets/TestStatsWidget.php
+++ filament-app/app/Filament/Widgets/TestStatsWidget.php
@@ -16,45 +16,45 @@ class TestStatsWidget extends StatsOverviewWidget
     {
         $totalUsers = User::query()->count('*');
         return [
-            Stat::make("Total Users", $totalUsers)
-                ->description("Total number of users of this years")
-                ->descriptionIcon(HeroIcon::ArrowUpLeft, IconPosition::Before)
-                ->chart(
-                    User::selectRaw("MONTH(created_at) as month, COUNT(*) as count", [])
-                        ->whereYear("created_at", now()->year)
-                        ->groupBy("month")
-                        ->orderBy("month")
-                        ->pluck("count")
-                        ->toArray()
-                )
-                ->descriptionColor("success")
-                ->color("success"),
-            Stat::make("Total Post", Post::count('*'))
-                ->description("Total number of Post of this years")
-                ->descriptionIcon(HeroIcon::ArrowUpLeft, IconPosition::Before)
-                ->chart(
-                    User::selectRaw("MONTH(created_at) as month, COUNT(*) as count", [])
-                        ->whereYear("created_at", now()->year)
-                        ->groupBy("month")
-                        ->orderBy("month")
-                        ->pluck("count")
-                        ->toArray()
-                )
-                ->descriptionColor("warning")
-                ->color("warning"),
-             Stat::make("Total Products", Product::count('*'))
-                ->description("Total number of Products of this years")
-                ->descriptionIcon(HeroIcon::ArrowUpLeft, IconPosition::Before)
-                ->chart(
-                    User::selectRaw("MONTH(created_at) as month, COUNT(*) as count", [])
-                        ->whereYear("created_at", now()->year)
-                        ->groupBy("month")
-                        ->orderBy("month")
-                        ->pluck("count")
-                        ->toArray()
-                )
-                ->descriptionColor("info")
-                ->color("info"),
+            // Stat::make("Total Users", $totalUsers)
+            //     ->description("Total number of users of this years")
+            //     ->descriptionIcon(HeroIcon::ArrowUpLeft, IconPosition::Before)
+            //     ->chart(
+            //         User::selectRaw("MONTH(created_at) as month, COUNT(*) as count", [])
+            //             ->whereYear("created_at", now()->year)
+            //             ->groupBy("month")
+            //             ->orderBy("month")
+            //             ->pluck("count")
+            //             ->toArray()
+            //     )
+            //     ->descriptionColor("success")
+            //     ->color("success"),
+            // Stat::make("Total Post", Post::count('*'))
+            //     ->description("Total number of Post of this years")
+            //     ->descriptionIcon(HeroIcon::ArrowUpLeft, IconPosition::Before)
+            //     ->chart(
+            //         User::selectRaw("MONTH(created_at) as month, COUNT(*) as count", [])
+            //             ->whereYear("created_at", now()->year)
+            //             ->groupBy("month")
+            //             ->orderBy("month")
+            //             ->pluck("count")
+            //             ->toArray()
+            //     )
+            //     ->descriptionColor("warning")
+            //     ->color("warning"),
+            //  Stat::make("Total Products", Product::count('*'))
+            //     ->description("Total number of Products of this years")
+            //     ->descriptionIcon(HeroIcon::ArrowUpLeft, IconPosition::Before)
+            //     ->chart(
+            //         User::selectRaw("MONTH(created_at) as month, COUNT(*) as count", [])
+            //             ->whereYear("created_at", now()->year)
+            //             ->groupBy("month")
+            //             ->orderBy("month")
+            //             ->pluck("count")
+            //             ->toArray()
+            //     )
+            //     ->descriptionColor("info")
+            //     ->color("info"),
         ];
     }
 }
